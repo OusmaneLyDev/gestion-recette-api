4ee7d4bee69f407c7fbf14d7d9f4e6ac3b96f56b
ousmane <ousmanely11@gmail.com>
2024-10-08 14:30:52 +0000

    Ajout du fichier .env.example

---
+++ gestion-recette-api.sql
@@ -0,0 +1,83 @@
+CREATE DATABASE  IF NOT EXISTS `gestion_recette` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `gestion_recette`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gestion_recette
+-- ------------------------------------------------------
+-- Server version	8.4.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nom` (`nom`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (4,'Boisson'),(3,'Dessert'),(1,'Entrée'),(2,'Plat');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `recettes`
+--
+
+DROP TABLE IF EXISTS `recettes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `recettes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titre` varchar(100) NOT NULL,
+  `ingredient` text NOT NULL,
+  `type` varchar(10) DEFAULT NULL,
+  `id_categorie` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_categorie_id` (`id_categorie`),
+  CONSTRAINT `fk_categorie_id` FOREIGN KEY (`id_categorie`) REFERENCES `categories` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recettes`
+--
+
+LOCK TABLES `recettes` WRITE;
+/*!40000 ALTER TABLE `recettes` DISABLE KEYS */;
+INSERT INTO `recettes` VALUES (1,'Lasagnes test','Pâtes, viande hachée, sauce tomate, fromage','Plat',NULL),(2,'Lasagnes','Pâtes, viande hachée, sauce tomate, fromage','Plat',2),(3,'Tarte aux pommes','Pommes, pâte brisée, sucre, cannelle','Dessert',3),(4,'Mojito','Rhum, menthe, citron, eau gazeuse, sucre','Boisson',4),(9,'bon','dessert','farime',NULL),(10,'Spaghetti test3 ','Pâte brisée, Lardons, ?ufs, Crème fraîche','dessert',3),(11,'bon','dessert','farime',NULL),(12,'bon','dessert','farime',NULL),(13,'bon','dessert','farime',NULL),(14,'bon','dessert','farime',NULL),(15,'bon','dessert','farime',NULL),(16,'bon','dessert','farime',NULL),(17,'bon','dessert','farime',NULL),(18,'Spaghetti test3 ','Pâte brisée, Lardons, ?ufs, Crème fraîche','dessert',3),(19,'Maccarouni','Tomate, pate','plat',2),(20,'Spaghetti test3 ','Pâte brisée, Lardons, ?ufs, Crème fraîche','dessert',3);
+/*!40000 ALTER TABLE `recettes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-08 14:24:49
